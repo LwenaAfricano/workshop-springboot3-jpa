6012548c7065b2832fc0c077a1b4572d9299ec80
Lwena Africano <lwliana1@gmail.com>
2024-09-18 10:21:03 +0100

    H2 database, test profile, JPA

--- src/main/java/com/javaacademy/course/resources/UserResource.java
+++ src/main/java/com/javaacademy/course/resources/UserResource.java
@@ -1,5 +1,20 @@
 package com.javaacademy.course.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javaacademy.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
 public class UserResource {
 
+	//metodo end pois para acessar os usuarios
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
 }
