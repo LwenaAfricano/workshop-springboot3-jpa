98a28b5a0f617ee230823b5b7f545fe1dbfb6f3c
Lwena Africano <lwliana1@gmail.com>
2024-09-19 00:44:40 +0100

    User delete

--- src/main/java/com/javaacademy/course/services/UserService.java
+++ src/main/java/com/javaacademy/course/services/UserService.java
@@ -31,4 +31,9 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	//deleçao do usuario
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 }
